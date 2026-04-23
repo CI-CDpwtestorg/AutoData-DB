83b0e70225a286c06d668a5076e027a232f6d35a
mkiznherman <Mark.Kiznherman@jazzpharma.com>
2026-04-23 03:08:29 -0600

    cicd test

---
+++ AutoData/dbo/Test.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Test]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Test] TEXT NULL, 
+    [createdAt] DATE NULL, 
+    [updatedAt] DATE NULL, 
+    [Description] NCHAR(10) NULL
+)
