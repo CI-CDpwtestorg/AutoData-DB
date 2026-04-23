5b3987d1e682fddf37cbadb25fef03cb9df3b88c
mkiznherman <Mark.Kiznherman@jazzpharma.com>
2026-04-23 07:44:36 -0600

    update test

--- AutoData/dbo/Test.sql
+++ AutoData/dbo/Test.sql
@@ -4,5 +4,6 @@
     [Test] TEXT NULL, 
     [createdAt] DATE NULL, 
     [updatedAt] DATE NULL, 
-    [Description] NCHAR(10) NULL
+    [Description] NCHAR(10) NULL, 
+    [add one] NCHAR(10) NULL
 )
